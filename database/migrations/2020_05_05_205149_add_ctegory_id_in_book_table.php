95f34987e5e352f00d9cc1bb45cf93cd2a06da45
Mariam Elfeky <mariamelfeky123@gmail.com>
2020-05-05 23:35:39 +0200

    migrate database

---
+++ database/migrations/2020_05_05_205149_add_ctegory_id_in_book_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddCtegoryIdInBookTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('books', function (Blueprint $table) {
+            $table->integer('category_id')->unsigned();
+            $table->foreign('category_id')
+                  ->references('id')->on('categories')
+                  ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('books', function (Blueprint $table) {
+            //
+        });
+    }
+}
---
+++ database/migrations/2020_05_05_211402_create_rate_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRateTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rates', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')
+                  ->references('id')->on('users')
+                  ->onDelete('cascade');
+            $table->integer('book_id')->unsigned();
+            $table->foreign('book_id')
+                   ->references('id')->on('books')
+                   ->onDelete('cascade');
+            $table->integer('rate')->unsigned();       
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('phones', function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
+            $table->dropForeign(['book_id']);
+            $table->dropColumn(['rate']);
+
+        });
+        // Schema::dropIfExists('rates');
+    }
+}
